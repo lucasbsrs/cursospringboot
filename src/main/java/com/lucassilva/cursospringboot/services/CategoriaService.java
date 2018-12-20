af5acec1aa245b051e2301f1c84ac8ad62faa5b7
Lucas Batista da Silva <lucasbsrs@gmail.com>
2018-12-19 23:22:19 -0200

    Ajustes no endpoint/categorias/id

--- src/main/java/com/lucassilva/cursospringboot/services/CategoriaService.java
+++ src/main/java/com/lucassilva/cursospringboot/services/CategoriaService.java
@@ -7,17 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.lucassilva.cursospringboot.domain.Categoria;
 import com.lucassilva.cursospringboot.repositories.CategoriaRepository;
+import com.lucassilva.cursospringboot.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
-}
 
+}
