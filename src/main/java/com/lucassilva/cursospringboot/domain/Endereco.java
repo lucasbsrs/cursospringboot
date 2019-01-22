4acccff6edea2f2c924ced88a311529bcfb153cb
Lucas Batista da Silva <lucasbsrs@gmail.com>
2019-01-22 17:21:10 -0200

    Alterações classe Endereço

--- src/main/java/com/lucassilva/cursospringboot/domain/Endereco.java
+++ src/main/java/com/lucassilva/cursospringboot/domain/Endereco.java
@@ -38,19 +38,6 @@ public class Endereco implements Serializable {
 
 	}
 
-	public Endereco(Integer id, String logradouro, String numero, String complemento, String bairro, String cep,
-			Cidade cidade, Cliente cliente) {
-		super();
-		this.id = id;
-		this.logradouro = logradouro;
-		this.numero = numero;
-		this.complemento = complemento;
-		this.bairro = bairro;
-		this.cep = cep;
-		this.cidade = cidade;
-		this.cliente = cliente;
-	}
-
 	public Integer getId() {
 		return id;
 	}
