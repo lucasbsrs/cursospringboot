7637e97fe26fceb0eed3b4309ddd02c10700a88f
Lucas Batista da Silva <lucasbsrs@gmail.com>
2019-01-25 17:17:20 -0200

    Validação com Bean Validation

--- src/main/java/com/lucassilva/cursospringboot/services/CategoriaService.java
+++ src/main/java/com/lucassilva/cursospringboot/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.lucassilva.cursospringboot.domain.Categoria;
+import com.lucassilva.cursospringboot.dto.CategoriaDTO;
 import com.lucassilva.cursospringboot.repositories.CategoriaRepository;
 import com.lucassilva.cursospringboot.services.exceptions.DataIntegrityException;
 import com.lucassilva.cursospringboot.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,11 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO categoriaDTO) {
+		return new Categoria(categoriaDTO.getId(), categoriaDTO.getNome());
+		
 	}
 
 }
