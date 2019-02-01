ad21eee5656edb54dcabae8eb567d4e98ecf8444
Lucas Batista da Silva <lucasbsrs@gmail.com>
2019-02-01 16:02:39 -0200

    Inserindo Pedido

--- src/main/java/com/lucassilva/cursospringboot/domain/PagamentoComCartao.java
+++ src/main/java/com/lucassilva/cursospringboot/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.lucassilva.cursospringboot.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.lucassilva.cursospringboot.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 
 	private static final long serialVersionUID = 1L;
--- src/main/java/com/lucassilva/cursospringboot/resources/PedidoResource.java
+++ src/main/java/com/lucassilva/cursospringboot/resources/PedidoResource.java
@@ -1,27 +1,50 @@
 package com.lucassilva.cursospringboot.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.lucassilva.cursospringboot.domain.Pedido;
 import com.lucassilva.cursospringboot.services.PedidoService;
 
 @RestController
-@RequestMapping(value="/pedidos")
+@RequestMapping(value = "/pedidos")
 public class PedidoResource {
-	
+
 	@Autowired
 	private PedidoService service;
-	
-	@GetMapping(value="/{id}")
+
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
 		Pedido obj = service.find(id);
-		
+
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+//	@GetMapping()
+//	public ResponseEntity<Page<Pedido>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+//			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+//			@RequestParam(value = "orderBy", defaultValue = "instante") String orderBy,
+//			@RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+//		Page<Pedido> list = service.findPage(page, linesPerPage, orderBy, direction);
+//		return ResponseEntity.ok().body(list);
+//	}
+
 }
